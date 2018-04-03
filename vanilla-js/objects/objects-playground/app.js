d6fb588907f683779c06ccdb000356562ea9fb36
glowsquid <contact@neontiles.com>
2018-04-03 15:28:55 +0200

    objects

--- vanilla-js/objects/objects-playground/app.js
+++ vanilla-js/objects/objects-playground/app.js
@@ -4,15 +4,25 @@ const pet = {
   type: "dog",
   breed: "German Shepherd",
   legs: 4,
+  age: 5,
   language: "Doglish",
+  isCat: false,
   speak: function() {
     return "woof";
   }
 }
 
-console.log(pet.speak());     // Dotted notation
+console.log(pet.speak());     // Dot notation
 console.log(pet["speak"]());  // Bracket notation
 
+
+// Updating data
+pet.age +=1;
+pet["age"] +=1;
+pet.breed = "Poodle";
+
+let extra = "dog years"
+pet[extra] = 35;
 
 // Limitations
 
@@ -33,3 +43,48 @@ const str = "name";
 
 console.log(limits.str);   // doesn't look for name
 console.log(limits[str]);  // does evaluate str and looks for "name"
+
+// two other ways to initialize objects
+const patient = {}
+patient.name = "Jessie Pinkman";
+patient.gender = "Male";
+patient.condition = "High as a kite";
+patient.city = "Albuquerque";
+
+const profile = new Object();
+profile.name = "Walter White";
+profile.alias = "Heisenberg";
+profile.age = 52;
+profile.occupation = "\"Chemist\"";
+
+// objects within objects
+const pets = {
+  cockatiel: {
+    name: "Mango",
+    age: 8,
+    pro: "Sings",
+    con: "Singing doesn't stop"
+  },
+  fish: {
+    name: "Bubbles",
+    age: 1,
+    pro: "Pretty",
+    con: "Boring"
+  }
+}
+
+console.log(pets.cockatiel.age)
+console.log(pets.fish["age"])
+
+
+const someObject = {
+  friends: [
+    {name: "a"},
+    {name: "b"},
+    {name: "c"}
+  ],
+  color: "pink",
+  isEvil: true
+};
+
+console.log(someObject.friends[2].name)
