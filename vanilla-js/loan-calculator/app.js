b7370839011cd2288e15e1e11fde7001861e8a90
glowsquid <contact@neontiles.com>
2018-04-29 23:24:31 +0200

    number-guesser

--- vanilla-js/loan-calculator/app.js
+++ vanilla-js/loan-calculator/app.js
@@ -1,9 +1,20 @@
 
 // listen for submit 
-document.getElementById("loan-form").addEventListener("submit", calculateResults);
+document.getElementById("loan-form").addEventListener("submit", function(e){
+  // hide results
+  document.getElementById("results").style.display = "none";
 
-function calculateResults(e){
-  console.log("csada")
+
+  // show loader
+  document.getElementById("loading").style.display = "block";
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
+
+function calculateResults(){
+  console.log("calculating")
   // UI vars
   const amount = document.getElementById("amount");
   const interest = document.getElementById("interest");
@@ -25,9 +36,42 @@ function calculateResults(e){
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
-  } else{
-    console.log("error");
-  }
 
-  e.preventDefault();
+    // show results
+    document.getElementById("results").style.display = "block";
+    // hide loader
+    document.getElementById("loading").style.display = "none";
+  } else{
+  // showError("Please check your numbers");
+    showError("Please check your numbers");
+  }
+}
+
+
+// show error
+function showError(error){
+  // hide results
+  document.getElementById("results").style.display = "none";
+  // hide loader
+  document.getElementById("loading").style.display = "none";
+
+  // create div
+  const errorDiv = document.createElement("div");
+  // get elements
+  const card = document.querySelector(".card");
+  const heading = document.querySelector(".heading");
+  // add class
+  errorDiv.className = "alert alert-danger";
+  // create text node & append to div
+  errorDiv.appendChild(document.createTextNode(error));
+  // insert error above heading
+  card.insertBefore(errorDiv, heading);
+  // clear error after 3 sec
+  setTimeout(clearError, 3000);
+
+}
+
+//clear error
+function clearError(){
+  document.querySelector(".alert").remove();
 }
---
+++ vanilla-js/number-guesser/app.js
@@ -0,0 +1,92 @@
+
+// game values
+let min = Math.ceil(Math.random() * 84),
+    max = min + Math.ceil(Math.random() * 10) + 5,
+    winningNum = getWinningNum(min, max),
+    guessesLeft = 3;
+
+// UI elements
+const game = document.getElementById("game"),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector("#guess-input"),
+      message = document.querySelector('.message');
+
+// assign UI min & max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// play again eventlistener
+game.addEventListener("mousedown", function(e){
+  if(e.target.className === "play-again"){
+    window.location.reload();
+  }
+});
+
+// listen for guess
+guessBtn.addEventListener("click", function(){
+  let guess = parseInt(guessInput.value);
+  console.log(guess);
+  // validate input
+  if(isNaN(guess) || guess < min || guess > max){
+    setMessage(`Please enter a number between ${min} and ${max}!`, "red");
+  } else if(guess === winningNum){
+    gameOver(true, `${winningNum} is correct! Congratulations!`)
+  } else{
+    guessesLeft -= 1;
+    if(guessesLeft === 0){
+      gameOver(false, `Sorry, you lost!. The correct number was ${winningNum}!`)
+    } else{
+      // wrong answer
+      guessInput.value = "";
+      if(guess < winningNum){
+        if(guessesLeft === 1){
+          setMessage(`Nope.. The number is larger than ${guess}. You have only one more guess`);
+        } else{
+          setMessage(`Nope.. The number is larger than ${guess}. You have ${guessesLeft} guesses left`);
+        }
+      } else{
+        if(guessesLeft === 1){
+          setMessage(`Not quite.. The number is smaller than ${guess}. You have only one guess left.`);
+        } else{
+          setMessage(`Not quite.. The number is smaller than ${guess}. You have ${guessesLeft} guesses left.`);
+        }
+        
+        
+      }
+    }
+
+
+
+    
+  }
+});
+
+function gameOver(won, msg){
+  let color;
+  won === true ? color = "green" : color = "red";
+
+  // disable input
+  guessInput.disabled = true;
+  // change border color
+  guessInput.style.borderColor = color;
+  // set text color
+  message.style.color = color;
+  // set win/lose message
+  setMessage(msg);
+  // play again
+  guessBtn.value = "Play again";
+  guessBtn.className += "play-again";
+}
+
+// get winning number
+function getWinningNum(min, max){
+  return Math.floor(Math.random(min, max)*(max-min+1)+min);
+}
+
+// set message
+function setMessage(msg, color){
+  message.style.color = color;
+  message.textContent = msg;
+}
