30981f5cc17d3a631f92d602f85ec7e4e119183a
glowsquid <koz@glowsquid.com>
2018-11-11 23:43:10 +0100

    post-section

--- mern/devconnector/client/src/App.js
+++ mern/devconnector/client/src/App.js
@@ -22,6 +22,8 @@ import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import NotFound from "./components/not-found/NotFound";
+import Posts from "./components/posts/Posts";
 
 import "./App.css";
 
@@ -93,6 +95,12 @@ class App extends Component {
                   component={AddEducation}
                 />
               </Switch>
+
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
+              </Switch>
+
+              <Route exact path="/404" component={NotFound} />
             </div>
             <Footer />
           </div>
