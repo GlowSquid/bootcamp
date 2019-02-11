d0ea1be570192d7c40ed018238f90e10929dee47
glowsquid <koz@glowsquid.com>
2019-02-11 22:31:45 +0100

    listing bookings

--- graphql/event-booker/client/src/pages/Bookings.js
+++ graphql/event-booker/client/src/pages/Bookings.js
@@ -1,8 +1,83 @@
 import React, { Component } from 'react';
+import AuthContext from '../context/auth-context';
+
+import Spinner from '../components/Spinner/Spinner';
 
 class BookingsPage extends Component {
+  state = {
+    isLoading: false,
+    bookings: []
+  };
+
+  static contextType = AuthContext;
+
+  componentDidMount() {
+    this.fetchBookings();
+  }
+
+  fetchBookings = () => {
+    this.setState({ isLoading: true });
+    const requestBody = {
+      query: `
+          query {
+            bookings {
+              _id
+              createdAt
+              event {
+                _id
+                title
+                date
+              }
+            }
+          }
+        `
+    };
+
+    fetch('http://localhost:5002/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + this.context.token
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          console.log('111');
+          throw new Error('Failed!');
+        }
+        return res.json();
+      })
+      .then(resData => {
+        const bookings = resData.data.bookings;
+        this.setState({ bookings: bookings, isLoading: false });
+        console.log('222');
+      })
+      .catch(err => {
+        console.log(err);
+        console.log('333');
+        this.setState({ isLoading: false });
+      });
+  };
+
   render() {
-    return <h1>Bookings Page</h1>;
+    return (
+      <React.Fragment>
+        <h1>Bookings</h1>
+        {this.state.isLoading ? (
+          <Spinner />
+        ) : (
+          <ul>
+            {this.state.bookings.map(booking => (
+              <li key={booking._id}>
+                {booking.event.title} -{' '}
+                {new Date(booking.createdAt).toLocaleDateString()}
+              </li>
+            ))}
+          </ul>
+        )}
+      </React.Fragment>
+    );
   }
 }
 
