5f5a9d65e34c961eb8d8346e2d6157ee87fc2d3d
glowsquid <koz@glowsquid.com>
2019-04-23 17:53:52 +0200

    pokemon game

---
+++ react/pokegame/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Pokegame from './Pokegame';
+
+function App() {
+  return (
+    <div>
+      <h1>PokeGame</h1>
+      <Pokegame />;
+    </div>
+  );
+}
+
+export default App;
